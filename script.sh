214dc88965e28805852689e1913164d127c036fb
Cheng Xue <edge26.doc.ic.ac.uk>
2014-01-28 12:22:59 +0000

    escjava2 command file

---
+++ script.sh
@@ -0,0 +1,5 @@
+escjava2 -VCLimit 5000000 -NoCautions bookings/*.java
+#-NoWarn IndexNegative -NoWarn ArrayStore
+#escjava2 -VCLimit 5000000 -NoCautions bookings/Seat.java
+#-Routine indexToNumber
+#-NoWarn Modifies -NoWarn IndexTooBig 
